3b6a9e0c7ca2015331384d7f190fe4fe99e4f1c8
Brad Cantrell <brad777mx@hotmail.com>
2019-04-22 14:24:40 -0700

    added more to sleep form

--- sleep-tracker/src/components/SignupForm.js
+++ sleep-tracker/src/components/SignupForm.js
@@ -2,21 +2,42 @@ import React from 'react';
 import axios from 'axios';
 
 export default class SignupForm extends React.Component{
-    constructor(){
-        super()
 
-        this.state = {
-            //
-        }
+    state = {
+        username: '',
+        password: ''
+    }
+
+    handleChange = e => {
+        this.setState({[e.target.name]: e.target.value})
+    }
+
+    login = e => {
+        e.preventDefault()
+        localStorage.setItem("username", this.state.username)
+        localStorage.setItem("password", this.state.password)
+        this.setState({username: '', password: ''})
     }
 
     render(){
         return(
             <div>
                 <h1>Sign In</h1>
-                <form>
-                    <p>Name: <input name='name' type="text"/></p>
-                    <p>Email: <input name='email' type="email"/></p>
+                <form onSubmit={e => this.login(e)}>
+                    <p>Name: <input 
+                                    name='name' 
+                                    type="text"
+                                    value={this.state.username} 
+                                    onChange={this.state.username} 
+                                />
+                                </p>
+                    <p>Email: <input 
+                                    name='email' 
+                                    type="email"
+                                    value={this.state.password} 
+                                    onChange={this.state.password} 
+                                />
+                            </p>
                     <button>Login</button>
                 </form>
             </div>
